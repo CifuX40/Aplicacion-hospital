f8b6581e2d0c471630027b467da63a3c30ecb4a4
Carlos Guerrero <ingenieria@affinityelectronics.com>
2024-10-25 08:38:00 +0200

    funcionamiento aplicación kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
 }
 
 android {
@@ -38,7 +39,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8c"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
 
     kotlinOptions {
