e15b143535e3c81f943e957daf38d67f1561d918
CifuX40 <carlos.cifuentes@live.u-tad.com>
2024-10-27 10:22:17 +0100

    27/10/2024

--- app/src/main/java/com/example/mardeluna/StartScreen.kt
+++ app/src/main/java/com/example/mardeluna/StartScreen.kt
@@ -3,18 +3,15 @@ package com.example.mardeluna
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import androidx.navigation.NavHostController
 
 @Composable
