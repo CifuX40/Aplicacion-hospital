dc314770629afb589f43595a106513e8a518f65c
CifuX40 <carlos.cifuentes@live.u-tad.com>
2024-12-07 10:23:21 +0100

    7/12/2024

--- app/src/main/java/com/example/mardeluna/view/Evita_600.kt
+++ app/src/main/java/com/example/mardeluna/view/Evita_600.kt
@@ -1,19 +1,17 @@
 package com.example.mardeluna.view
 
-import android.util.Log
-import androidx.compose.foundation.Image
+import android.util.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import coil.compose.rememberAsyncImagePainter
-import com.google.firebase.storage.FirebaseStorage
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.layout.*
+import androidx.compose.ui.unit.*
+import androidx.navigation.*
+import coil.compose.*
+import com.google.firebase.storage.*
 
 @Composable
 fun Evita600(navController: NavHostController) {
